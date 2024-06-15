a17e94d46cc930d0656ba54a51bf9849e2a560c1
fanbozhou <1033866383@qq.com>
2024-06-15 19:13:41 +0800

    operate image in memory, html img src=base64

--- traverse_test.py
+++ traverse_test.py
@@ -1,3 +1,5 @@
+import base64
+import io
 import json
 import os
 import random
@@ -8,6 +10,8 @@ import webbrowser
 import concurrent.futures
 from datetime import datetime
 
+import cv2
+import numpy as np
 from PIL import Image, ImageDraw
 from jinja2 import Environment
 from playwright.sync_api import sync_playwright
@@ -60,18 +64,15 @@ template_string = """
 
 
 class TraverseTest(object):
-    def __init__(self, target_url, output_dir):
+    def __init__(self, target_url):
         self.target_url = target_url
         self.ele_list = []
         self.operations = []
-        self.tmp_path = None
-        self.output_dir = output_dir
-        if not os.path.exists(self.output_dir):
-            os.makedirs(self.output_dir)
+        self.base_img_byte = None
 
-    def get_image_touchability(self, image_path):
+    def get_image_touchability(self, image):
         """发送图像到服务并获取响应"""
-        return get_ui_infer_by_all_model(image_path)
+        return get_ui_infer_by_all_model(image)
 
     def open_target_web_and_capture(self):
         """打开目标网页，捕获屏幕截图并上传"""
@@ -83,10 +84,10 @@ class TraverseTest(object):
                 page.wait_for_load_state("networkidle", timeout=30000)
             except:
                 pass
-            self.tmp_path = "{0}.png".format(random.randint(0, 999999))
-            img_bytes = page.screenshot(path=self.tmp_path)
+            self.base_img_byte = page.screenshot()
             page.close()
-            resp = self.get_image_touchability(self.tmp_path)
+            resp = self.get_image_touchability(
+                cv2.imdecode(np.frombuffer(self.base_img_byte, np.uint8), cv2.IMREAD_COLOR))
             print(json.dumps(resp))
             self.ele_list = resp
         self.traverse_touchable()
@@ -101,20 +102,20 @@ class TraverseTest(object):
         with open("test_report.html", "w", encoding="utf-8") as f:
             f.write(output_html)
 
-    def copy_and_mark(self, input_image_path, output_image_path, point):
+    def copy_and_mark(self, point):
         # 打开原始图片
-        image = Image.open(input_image_path)
+        image_stream = io.BytesIO(self.base_img_byte)
+        image = Image.open(image_stream)
         # 复制图片
         image_copy = image.copy()
-
         # 标记点位
         draw = ImageDraw.Draw(image_copy)
         # 标记点位，增加线条宽度和改变颜色为红色
         draw.line((point[0] - 20, point[1], point[0] + 20, point[1]), fill=(255, 0, 0), width=10)  # 垂直
         draw.line((point[0], point[1] - 20, point[0], point[1] + 20), fill=(255, 0, 0), width=10)  # 水平
-
-        # 保存新图片
-        image_copy.save(output_image_path)
+        buffered = io.BytesIO()
+        image_copy.save(buffered, format='PNG')  # 选择一个适当的格式，例如JPEG或PNG
+        return base64.b64encode(buffered.getvalue()).decode('utf-8')
 
     def traverse_touchable(self):
         with concurrent.futures.ThreadPoolExecutor() as executor:
@@ -131,7 +132,7 @@ class TraverseTest(object):
                             pass
                         x = (i.get("elem_det_region")[0] + i.get("elem_det_region")[2]) / 2
                         y = (i.get("elem_det_region")[1] + i.get("elem_det_region")[3]) / 2
-                        self.copy_and_mark(self.tmp_path, "{1}/tmp_{0}_a.png".format(index, self.output_dir), (x, y))
+                        mark_image_base64 = self.copy_and_mark((x, y))
                         print(index, i)
                         page.mouse.click(x, y)
                         try:
@@ -139,22 +140,18 @@ class TraverseTest(object):
                         except:
                             pass
                         time.sleep(5)
-                        page.screenshot(path="{1}/tmp_{0}_b.png".format(index, self.output_dir))
+                        click_mark_image_base64 = base64.b64encode(page.screenshot()).decode('utf-8')
                         page.close()
                         self.operations.append(
                             {"info": "click: {0}".format(i.get("elem_det_type")),
-                             "before_image": "{1}/tmp_{0}_a.png".format(index, self.output_dir),
-                             "after_image": "{1}/tmp_{0}_b.png".format(index, self.output_dir)}, )
+                             "before_image": "data:image/png;base64,{0}".format(mark_image_base64),
+                             "after_image": "data:image/png;base64,{0}".format(click_mark_image_base64)}, )
 
                 except:
                     traceback.print_exc()
 
             tasks = [executor.submit(run, index, i) for index, i in enumerate(self.ele_list)]
             done, not_done = concurrent.futures.wait(tasks)
-            try:
-                os.remove(self.tmp_path)
-            except:
-                traceback.print_exc()
 
 
 if __name__ == "__main__":
@@ -164,15 +161,13 @@ if __name__ == "__main__":
             target_url = sys.argv[1]
     except:
         pass
-    current_time = datetime.now()
-    formatted_time = current_time.strftime('%Hh%Mm%Ss')
-    output_task_path = "task_{0}".format(formatted_time)
     print("target_url: {0}".format(target_url))
-    print("output_task_path: {0}".format(output_task_path))
     traverse_test = TraverseTest(
         target_url=target_url,
-        output_dir=output_task_path
     )
     traverse_test.open_target_web_and_capture()
     traverse_test.gen_report()
-    webbrowser.open("file://" + os.path.abspath("test_report.html"))
+    try:
+        webbrowser.open("file://" + os.path.abspath("test_report.html"))
+    except:
+        pass
